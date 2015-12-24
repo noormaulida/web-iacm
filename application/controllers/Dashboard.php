bb7d160e37d04671c97131ec72164022b345de3c
Noor Maulida <noormaulida.018@gmail.com>
2015-12-25 02:33:05 +0700

    #2 Check auth in dashboard controller

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,8 +1,17 @@
 <?php
 class Dashboard extends CI_Controller {
 
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     public function index()
     {
+        if (!is_admin_session()) {
+            echo "You're not authorize";
+            return;
+        }
     	$this->load->view('dashboard/_include/header');
     	$this->load->view('dashboard/index');
     	$this->load->view('dashboard/_include/footer');
