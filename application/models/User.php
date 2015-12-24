29e348afe1612a6aecf60719f0892b12dd0c151b
Noor Maulida <noormaulida.018@gmail.com>
2015-12-24 23:53:42 +0700

    #1 User model with field

--- application/models/User.php
+++ application/models/User.php
@@ -1,18 +1,27 @@
 <?php
 class User extends CI_Model {
+    var $full_name = '';
+    var $nick_name = '';
+    var $cm_generation = '';
+    var $email = '';
+    var $password = '';
+    var $address = '';
+    var $phone = '';
+    var $date_of_birth = '';
+    var $company = '';
+    var $occupation = '';
+    var $institution = '';
+    var $avatar = '';
+    var $login_count = '';
+    var $is_admin = '';
+    var $validated_with = '';
+    var $validated_at = '';
+    var $created_at = '';
+    var $updated_at = '';
 
     public function __construct()
     {
+        parent::__construct();
         $this->load->database();
     }
-
-    public function get_user($username, $password)
-    {
-        $this->db->select("*");
-        $this->db->from('users');
-        $this->db->where("email", $email);
-        $this->db->where("password", $password);
-        $query = $this->db->get();
-        return $query->row_array();
-   }
 }
