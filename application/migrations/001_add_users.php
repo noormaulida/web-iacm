89309d0362b68f0a77a48270ec4e38f0872a1758
Noor Maulida <noormaulida.018@gmail.com>
2015-12-27 22:23:14 +0700

    #12 Add super admin data when migrating

--- application/migrations/001_add_users.php
+++ application/migrations/001_add_users.php
@@ -27,6 +27,7 @@ class Migration_Add_users extends CI_Migration {
          */
         public function up()
         {
+            $this->load->model('user');
             $this->dbforge->add_field('id');
             $this->dbforge->add_field(array(
                 'full_name' => array(
@@ -119,6 +120,27 @@ class Migration_Add_users extends CI_Migration {
                 ),
             ));
             $this->dbforge->create_table('users');
+            $data = array(
+                    "full_name" => "Super Admin",
+                    "nick_name" => "Super Admin",
+                    "cm_generation" => 1,
+                    "email" => "momo@suitmedia.com", // ubah data ini
+                    "password" => $this->user->_hash('superadmin098'), // dan ini
+                    "address" => "default",
+                    "phone" => "default",
+                    "date_of_birth" => "",
+                    "company" => "",
+                    "occupation" => "",
+                    "institution" => "",
+                    "avatar" => "assets/uploads/avatar/default.png",
+                    "login_count" => 0,
+                    "is_admin" => 1,
+                    "created_at" => date('Y-m-d H:i:s'),
+                    "updated_at" => date('Y-m-d H:i:s'),
+                    "validated_at" => date('Y-m-d H:i:s'),
+                    "validated_with" => 1
+            );
+            $this->user->store($data);
         }
 
         public function down()
